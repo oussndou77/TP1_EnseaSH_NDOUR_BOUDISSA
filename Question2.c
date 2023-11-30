0f0066edfb4973c1734b0d03ffcc467227bb69d2
oussndou77 <148482151+oussndou77@users.noreply.github.com>
2023-11-30 12:10:12 +0100

    Update Question2.c
    Signed-off-by: oussndou77 <148482151+oussndou77@users.noreply.github.com>

--- Question2.c
+++ Question2.c
@@ -18,28 +18,29 @@ int main(){
 
 // on crée une boucle infinie pour simuler un prompt de shell continu.
     while(STDOUT_FILENO){   
-        write(STDOUT_FILENO,"%",strlen("%"));                                                   // permet de retourner à la ligne suivante après l'exécution d’une commande.
-        commande_size= read(STDIN_FILENO,buf,BUFSIZE);                                             //on utilise la fonction read pour lire une commande depuis l'entrée standard
-        exeCommande(buf,commande_size);                                                                      // on excute la commande avec la foncton exeCommande
+        write(STDOUT_FILENO,"%",strlen("%")); // permet de retourner à la ligne suivante après l'exécution d’une commande.
+        commande_size= read(STDIN_FILENO,buf,BUFSIZE); //on utilise la fonction read pour lire une commande depuis l'entrée standard
+        exeCommande(buf,commande_size);// on excute la commande avec la foncton exeCommande
     }
 
     return 0;
 }
 
-void exeCommande(char *buf,ssize_t commande_size){                                               // fonction excution qui prend un tableau de caractères buf (la commande)et la taille de la commande commande_size comme paramètr
+void exeCommande(char *buf,ssize_t commande_size){ // fonction excution qui prend un tableau de caractères buf (la commande)et la taille de la commande commande_size comme paramètr
 
     int pid;
     int status;
-    buf[commande_size - 1] = '\0';                                                               //   “enter” est pris en compte avec le read . Il faut donc le retirer pour ne pas fausser execlp.Pour cela n remplace ce terme par :\0
+    buf[commande_size - 1] = '\0'; // “enter” est pris en compte avec le read . Il faut donc le retirer pour ne pas fausser execlp.Pour cela n remplace ce terme par :\0
 
 
-    pid=fork();                                                                                 // On appelle la fonction système fork pour créer un nouveau processus. Le PID du processus fils est retourné dans le processus parent et 0 dans le processus fils.
+    pid=fork(); / On appelle la fonction système fork pour créer un nouveau processus. Le PID du processus fils est retourné dans le processus parent et 0 dans le processus fils.
 
-    if (pid!= 0){                                                                               // condition pour le processus parent
-        wait(&status);                                                                    //on utilise la fonction wait pour attendre la fin du processus fils et récupérer son statut.
+        // condition pour le processus parent
+    if (pid!= 0){  
+        wait(&status); //on utilise la fonction wait pour attendre la fin du processus fils et récupérer son statut.
     }
-    else{                                                                                      //   pour le processus fils.
-        execlp(buf,buf,(char *)NULL);//On utilise la fonction execlp pour exécuter la commande spécifiée par buf. Si cela réussit, le processus fils se termine après l'exécution de la commande.
+    else{                                                                                      
+        execlp(buf,buf,(char *)NULL); //On utilise la fonction execlp pour exécuter la commande spécifiée par buf. Si cela réussit, le processus fils se termine après l'exécution de la commande.
         exit(EXIT_FAILURE);
 
     }
