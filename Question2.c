e58ec29235e3fffa1d66d813465d80afcacf64a1
oussndou77 <148482151+oussndou77@users.noreply.github.com>
2023-12-02 20:46:44 +0100

    Update Question2.c

--- Question2.c
+++ Question2.c
@@ -1,70 +1,46 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/wait.h>
-
 #define BUFSIZE 1096
 
-// Function to display the welcome message and prompt
-void displayWelcomeMessage() {
-    char message[BUFSIZE] = "Welcome to the ENSEA Shell.\nTo exit, type 'exit'.\nenseash %\n";
-    write(STDOUT_FILENO, message, strlen(message));
-}
+//Question 2:
 
-// Function to execute a simple command
-void executeCommand(char* command) {
-    // Create a child process
-    pid_t pid = fork();
+void exeCommande(char *buf, ssize_t commande_size);
 
-    if (pid == -1) {
-        perror("Error creating child process");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        // This is the child process
+int main(){
+    char welcome[BUFSIZE]="enseah% fortune\n Today is what happened to yesterday.\n enseah" ; // création d'un tableau de caractère welcome avec le message de bienvenue
+    char buf[BUFSIZE]; // déclaration d'un tableau de caractères pour stocker les commandes entrées par l'utilisateur.
+    ssize_t commande_size; // variable pour stocker la taille de la commande lue par read
 
-        // Execute the command
-        if (execlp(command, command, (char *)NULL) == -1) {
-            perror("Error executing command");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        // This is the parent process
+    write(STDOUT_FILENO,welcome,strlen(welcome));   // on utilise write pour afficher le message de bienvenue (welcome) sur la sortie standard
 
-        // Wait for the child process to finish
-        int status;
-        waitpid(pid, &status, 0);
-    }
-}
-
-int main() {
-    char input[BUFSIZE];
-
-    // Display the welcome message
-    displayWelcomeMessage();
-
-    while (1) {
-        // Display the prompt
-        write(STDOUT_FILENO, "enseash % ", 11);
-
-        // Read the user input
-        if (read(STDIN_FILENO, input, sizeof(input)) == -1) {
-            perror("Error reading the command");
-            exit(EXIT_FAILURE);
-        }
-
-        // Remove the newline character (if it exists)
-        input[strcspn(input, "\n")] = '\0';
-
-        // Check if the user wants to exit
-        if (strcmp(input, "exit") == 0) {
-            write(STDOUT_FILENO, "Goodbye!\n", 9);
-            break;
-        }
-
-        // Execute the entered command
-        executeCommand(input);
+// on crée une boucle infinie pour simuler un prompt de shell continu.
+    while(STDOUT_FILENO){   
+        write(STDOUT_FILENO,"%",strlen("%"));  // permet de retourner à la ligne suivante après l'exécution d’une commande.
+        commande_size= read(STDIN_FILENO,buf,BUFSIZE); //on utilise la fonction read pour lire une commande depuis l'entrée standard
+        exeCommande(buf,commande_size); // on excute la commande avec la foncton exeCommande
     }
 
     return 0;
+}
+
+void exeCommande(char *buf,ssize_t commande_size){  // fonction excution qui prend un tableau de caractères buf (la commande)et la taille de la commande commande_size comme paramètr
+
+    int pid;
+    int status;
+    buf[commande_size - 1] = '\0'; //  “enter” est pris en compte avec le read . Il faut donc le retirer pour ne pas fausser execlp.Pour cela n remplace ce terme par :\0
+
+
+    pid=fork();// On appelle la fonction système fork pour créer un nouveau processus. Le PID du processus fils est retourné dans le processus parent et 0 dans le processus fils.
+
+// condition pour le processus parent
+    if (pid!= 0){                                                                               
+        wait(&status); //on utilise la fonction wait pour attendre la fin du processus fils et récupérer son statut.
+    }
+    else{ //   pour le processus fils.
+        execlp(buf,buf,(char *)NULL); //On utilise la fonction execlp pour exécuter la commande spécifiée par buf. Si cela réussit, le processus fils se termine après l'exécution de la commande.
+        exit(EXIT_FAILURE);
+    }
 }
