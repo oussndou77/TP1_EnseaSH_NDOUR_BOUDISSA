d2157cf74a4bbb9b00de2647bcac21564b67ccd3
oussndou77 <148482151+oussndou77@users.noreply.github.com>
2023-12-02 16:50:59 +0100

    Update Question1.c
    Signed-off-by: oussndou77 <148482151+oussndou77@users.noreply.github.com>

--- Question1.c
+++ Question1.c
@@ -4,12 +4,15 @@
 
 #define BUFSIZE 1096
 
-//Question 1 : Affichage d’un message d’accueil, suivi d’un prompt simple
-
-int main(){
-    char buf [BUFSIZE]="Bienvenue dans le shell ENSEA .\n Pour quitter, taper 'exit' .\n enseash % \n"; // Création d'un tableau de caractère , on rentre dedans le message qu'on veut afficher
-    write(STDOUT_FILENO, buf, strlen(buf)); // On utilise la fonction write pour écrire le contenu du buffer sur la sortie standard (stdout) liée à l affichage de la console
-    return 0; // Renvoie 0 en cas de résussite
-
+// Function to display the welcome message and prompt
+void displayWelcomeMessage() {
+    char message[BUFSIZE] = "Welcome to the ENSEA Shell.\nTo exit, type 'exit'.\nenseash %\n";
+    write(STDOUT_FILENO, message, strlen(message));
 }
 
+int main() {
+    // Call the function to display the welcome message
+    displayWelcomeMessage();
+
+    return 0;
+}
